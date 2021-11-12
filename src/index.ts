4b858ff6ac52b53c6b071d1c3f4b73191194a4f7
Mateusz Marzęcki <mmarzecki@atlassian.com>
2021-11-13 00:35:15 +0100

    Added simple UI for routes preview

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import dontenv from 'dotenv';
 import express from 'express';
+import ejs from 'ejs';
+import path from 'path';
+
 import { SERVER_CONFIG } from '../config';
 
 import Airports from './Airports';
@@ -9,29 +12,41 @@ dontenv.config();
 
 const app = express();
 
+app.set("views", path.join( __dirname, "views" ));
+app.engine('html', ejs.renderFile);
+
+const polandCenterCords = {
+	lat: 52.196283,
+	lon: 19.356571,
+}
+
+const airPorts = new Airports(350, 30);
+const routes = new Routes();
+
+const buildViewFilePath = (fileName: string): string => {
+	return path.join(__dirname, '..', 'views', fileName);
+}
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
+	airPorts
+	.getAirportsByCoorindates(polandCenterCords.lat, polandCenterCords.lon)
+	.then(airports => {
+		res.render(buildViewFilePath('index.html'), {
+			airports: JSON.stringify(airports),
+		});
+	});
+});
+
+app.get('/routes/:icao', (req, res) => {
+	const icao = req.params.icao;
+
+	routes.getAirportRoutes(icao).then(response => {
+		res.send(response);
+	})
+});
+
+app.get('*', (req, res) => {
+	res.redirect('/');
 });
   
 app.listen(process.env.SERVER_PORT || SERVER_CONFIG.DEFAULT_PORT);
-
-
-
-// const polandCenterCords = {
-//     lat: 52.196283,
-//     lon: 19.356571,
-// }
-
-// const airPorts = new Airports(350, 30);
-// const routes = new Routes();
-
-// routes.getAirportRoutes('EPGD').then(response => {
-//     console.dir(response, { depth: null });
-// })
-
-// airPorts
-//     .getAirportsByCoorindates(polandCenterCords.lat, polandCenterCords.lon)
-//     .then(airports => {
-//         console.dir(airports, { depth: null });
-//     });
